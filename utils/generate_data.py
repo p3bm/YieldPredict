915b4b3e7a2836a5038c6f8381cbb4cdad94cdc8
Pengxiang Hua <hpx@mail.ustc.edu.cn>
2024-09-27 23:07:45 +0800

    Update generate_data.py

--- utils/generate_data.py
+++ utils/generate_data.py
@@ -21,23 +21,23 @@ def main():
         excel = xlrd2.open_workbook(strs)
         sheet = excel.sheet_by_index(0)
         
-        if args.representation == "morgan_fp":
+        if representation == "morgan_fp":
             flattened_encoding_of_dataset, yields = mfp.encode_dataset(sheet, num_columns_of_sheet, 2048)
-        elif args.representation == "Mordred":
+        elif representation == "Mordred":
             flattened_encoding_of_dataset, yields = mdr_descriptors.encode_dataset(sheet, num_columns_of_sheet)
         '''
-        elif args.representation == "rdkit":
+        elif representation == "rdkit":
             flattened_encoding_of_dataset, yields = fprdkit.encode_dataset(sheet, num_columns_of_sheet)
-        elif args.representation == "rxnfp":
+        elif representation == "rxnfp":
             flattened_encoding_of_dataset, yields = myrxnfp.encode_dataset(sheet, num_columns_of_sheet, 5)
-        elif args.representation == 'atompair':
+        elif representation == 'atompair':
             flattened_encoding_of_dataset, yields = ap.encode_dataset(sheet, num_columns_of_sheet)
             print(flattened_encoding_of_dataset.shape)
-        elif args.representation == 'torsion':
+        elif representation == 'torsion':
             flattened_encoding_of_dataset, yields = tfp.encode_dataset(sheet, num_columns_of_sheet)
-        elif args.representation == 'ECFP':
+        elif representation == 'ECFP':
             flattened_encoding_of_dataset, yields = ECFP.encode_dataset(sheet, num_columns_of_sheet)
-        elif args.representation == 'FCFP':
+        elif representation == 'FCFP':
             flattened_encoding_of_dataset, yields = FCFP.encode_dataset(sheet, num_columns_of_sheet)
         '''
         np.savez(type + '_' + representation + ".npz", train_data=flattened_encoding_of_dataset, train_labels=yields)
